3cac6e486b0c8f0aad9358261135708c0cbde2fb
Nikita Tyukachev <naensamble@gmail.com>
2022-03-24 15:51:23 +0300

    SQL lesson 10 update

--- lesson_10_homework.sql
+++ lesson_10_homework.sql
@@ -30,3 +30,62 @@ GROUP BY "date", bank, gender, age_category
 
 
 -- Задание 2
+
+WITH t1 AS
+    (
+    SELECT cd.client_id
+        , bank
+        , actual_from_date
+        , MAX(salary) AS max_salary
+    FROM clnt_aggr ca
+    JOIN clnt_data cd
+        ON ca.client_id = cd.client_id
+    GROUP BY client_id, bank, actual_from_date
+    ), t2 AS
+    (
+    SELECT cd.client_id
+        , bank
+        , actual_from_date
+        , max_salary
+        , age
+    FROM t1
+    JOIN clnt_data cd
+        ON cd.client_id = t1.client_id
+    )
+SELECT client_id
+    , bank
+    , actual_from_date
+    , max_salary
+    , age
+FROM t2
+JOIN (
+    SELECT client_id
+        , MAX(actual_from_date) AS actual_from_date
+    FROM clnt_data cd
+    GROUP BY client_id
+    ) s
+ON s.client_id = t2.client_id
+    AND s.actual_from_date = t2.actual_from_date
+
+
+
+-- Задание 3
+
+SELECT client_id
+FROM
+(
+    SELECT DISTINCT cd.client_id
+        , actual_from_date
+    FROM clnt_aggr ca
+    JOIN clnt_data cd
+        ON cd.client_id = ca.client_id
+    WHERE pos_amt > (SELECT AVG(pos_amt) FROM clnt_aggr)
+) s 
+JOIN (
+    SELECT client_id
+        , MAX(actual_from_date) AS actual_from_date
+    FROM clnt_data cd
+    GROUP BY client_id
+    ) s2
+ON s.client_id = s2.client_id
+    AND s.actual_from_date = s2.actual_from_date
